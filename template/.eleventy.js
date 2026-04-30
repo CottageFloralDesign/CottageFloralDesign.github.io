ca2d53aaf95e6805c1a2b23ba1905fbf7d6040eb
Snicker7 <snic9004@gmail.com>
2026-04-30 14:15:17 -0600

    chore(template): scaffold Eleventy project

---
+++ template/.eleventy.js
@@ -0,0 +1,19 @@
+module.exports = function(eleventyConfig) {
+  // Pass-through static assets
+  eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("js");
+  eleventyConfig.addPassthroughCopy("assets");
+  eleventyConfig.addPassthroughCopy("admin");
+
+  return {
+    dir: {
+      input: ".",
+      output: "_site",
+      includes: "_includes",
+      data: "_data"
+    },
+    templateFormats: ["njk", "md", "html"],
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk"
+  };
+};
